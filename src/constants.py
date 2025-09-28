3543294e4989724233e2220610af4abf73def722
Lyuboslav Karev <lyubo.karev@gmail.com>
2025-09-28 12:31:27 +0300

    Add constants

---
+++ src/constants.py
@@ -0,0 +1,8 @@
+import os
+
+VERSION = "1.0.0"
+
+WORK_DIR = os.path.join("/tmp", "homework_deployer")
+
+SOURCE_REPO_DIR = os.path.join(WORK_DIR, "source_repo")
+DESTINATION_REPO_DIR = os.path.join(WORK_DIR, "destination_repo")
