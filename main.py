3363ad5fb5ad8cc7be3267ecaca918acb064fa75
Lyuboslav Karev <lyubo.karev@gmail.com>
2025-09-28 19:39:11 +0300

    Add sample main

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
+"""
+Main module
+"""
+
+from datetime import datetime
+from src.event import Event
+from src.executor import execute
+
 if __name__ == "__main__":
-    print("Hello world")
+    e = Event(
+        id=1,
+        name="Test Event",
+        description="This is a test event",
+        origin="https://github.com/lyubolp/tmp-2",
+        destination="https://github.com/lyubolp/tmp-1",
+        date=datetime.now(),
+        patterns=[("baz.txt", "temp/baz-2.txt"), ("*.py", None)],
+    )
+
+    execute(e)
